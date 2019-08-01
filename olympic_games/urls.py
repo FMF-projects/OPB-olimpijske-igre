e3809e04439338e3844021c7d09602b08e66415a
Klementina Pirc <klementina.pirc@student.fmf.uni-lj.si>
2019-08-01 09:35:08 +0200

    dropdown get selected values

--- olympic_games/urls.py
+++ olympic_games/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from . import views
 
-app_name = 'olympic_games'
+app_name = 'olymipic_games'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('results/', views.ResultsPageView.as_view(), name='results'),
